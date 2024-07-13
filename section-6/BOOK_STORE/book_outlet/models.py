8286977dbd81fd0fed7c20e7b6a86ea8ef5a7445
CodeCraftKiran <codecraftkiran@gmail.com>
2024-07-13 13:55:07 +0530

    complete section 6

--- section-6/BOOK_STORE/book_outlet/models.py
+++ section-6/BOOK_STORE/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class books(models.Model):
@@ -7,6 +9,17 @@ class books(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=50)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)    
+    # db_index basically help in finding data faster.
+    # when we use any field offen to retrive data we should consider adding db_index there.
+    
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- section-6/BOOK_STORE/book_outlet/views.py
+++ section-6/BOOK_STORE/book_outlet/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
+from .models import books
 # Create your views here.
+def index(request):
+    all_books = books.objects.all().order_by("-title")  # minus => -title is use for descending
+    num_books = all_books.count()
+    avg_rating = all_books.aggregate(Avg("rating"))
+    
+    return render(request, "book_outlet/index.html", {
+        "all_books": all_books,
+        "total_num_of_books": num_books,
+        "average_rating":avg_rating,
+    })
+    
+def book_detail(request, slug):
+    # book = books.objects.get(pk=id) 
+    book = get_object_or_404(books, slug=slug) 
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
