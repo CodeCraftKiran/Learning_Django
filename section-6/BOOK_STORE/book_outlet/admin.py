6ebdcb7ef216df743822b4b5626c6db34109f110
CodeCraftKiran <codecraftkiran@gmail.com>
2024-07-13 17:20:04 +0530

    section 6 & 7

--- section-6/BOOK_STORE/book_outlet/admin.py
+++ section-6/BOOK_STORE/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import books
+
+class booksAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author", )
+
 # Register your models here.
+admin.site.register(books, booksAdmin)
--- section-6/BOOK_STORE/book_outlet/models.py
+++ section-6/BOOK_STORE/book_outlet/models.py
@@ -16,9 +16,9 @@ class books(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
         
     
     def __str__(self):
