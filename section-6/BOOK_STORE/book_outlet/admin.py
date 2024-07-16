00b4bda87e3c2f5263899732f1889a1bdc905a6f
CodeCraftKiran <codecraftkiran@gmail.com>
2024-07-16 20:00:07 +0530

    complete section 6,7 and 8

--- section-6/BOOK_STORE/book_outlet/admin.py
+++ section-6/BOOK_STORE/book_outlet/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 
-from .models import books
+from .models import books, Author,Address, Country
 
 class booksAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug":("title",)}
     list_filter = ("author", "rating",)
     list_display = ("title", "author", )
+    
 
 # Register your models here.
 admin.site.register(books, booksAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
--- section-6/BOOK_STORE/book_outlet/models.py
+++ section-6/BOOK_STORE/book_outlet/models.py
@@ -3,18 +3,55 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 # Create your models here.
+class Country(models.Model):
+    country = models.CharField(max_length=50)
+    codr= models.CharField(max_length=2)
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+        
+    def __str__(self):
+        return self.country
+        
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city=models.CharField(max_length=50)
+    
+    def full_address(self):
+        return f"{self.street}, {self.postal_code}, {self.city}."
+    
+    def __str__(self):
+        return self.full_address()
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name= models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
 
 
 class books(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="related_books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)    
+    slug = models.SlugField(default="", null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
     # db_index basically help in finding data faster.
     # when we use any field offen to retrive data we should consider adding db_index there.
     
