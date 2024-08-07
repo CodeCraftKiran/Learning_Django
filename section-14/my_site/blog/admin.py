04dff07b9592a91b1c55d6030b5144109a24292a
CodeCraftKiran <codecraftkiran@gmail.com>
2024-08-07 21:38:01 +0530

    section 14 complete

--- section-14/my_site/blog/admin.py
+++ section-14/my_site/blog/admin.py
@@ -7,8 +7,12 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date")
     list_display = ("title", "date", "author")
     prepopulated_fields = {"slug": ("title", )}
+    
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "related_post")    
 
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comments)
+admin.site.register(Comments, CommentAdmin)
--- section-14/my_site/blog/urls.py
+++ section-14/my_site/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting_page"),
     path("posts", views.AllPostView.as_view(), name="posts_page"),
-    path("path/<slug:slug>", views.SinglePostView.as_view(), name="post_details_page")
+    path("path/<slug:slug>", views.SinglePostView.as_view(), name="post_details_page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
