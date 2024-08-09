80dffbb817e929abbf5a9d8508b93357ea20f4a8
CodeCraftKiran <codecraftkiran@gmail.com>
2024-08-09 20:54:22 +0530

    summary course

--- section-16/course_site/meetups/views.py
+++ section-16/course_site/meetups/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    meet = [
-        {"title": "first meet up"},
-        {"title": "second meet up"}
-    ]
+    meetups = [
+        {
+            "title": "A First meetup",
+            "location": "New York",
+            "slug": "a-first-meetup",
+        },
+        {
+            "title": "A Second meetup",
+            "location": "Pari",
+            "slug": "a-second-meetup",
+        },
+    ]    
     return render(request, "meetups/index.html",{
-        "meetus":meet
+        "show_meetups":True,
+        "meetups": meetups
     })
