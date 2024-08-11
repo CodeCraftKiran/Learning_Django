9af30e45afb47f5ea86d7bac678769d3c1b71337
CodeCraftKiran <codecraftkiran@gmail.com>
2024-08-11 21:16:58 +0530

    COURSE COMPLETE

--- section-16/course_site/meetups/urls.py
+++ section-16/course_site/meetups/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("meetups/", views.index, name="all-meetups"),
-    path("meetups/<slug:meetup_slug>", views.meetup_details, name="meetup-details"),
-]
 
+urlpatterns = [
+    path("", views.index, name="all-meetups"),
+    path("<slug:meetup_slug>success", views.confirm_registration, name="confirm-registration"),
+    path("<slug:meetup_slug>", views.meetup_details, name="meetup-details"),
+]
